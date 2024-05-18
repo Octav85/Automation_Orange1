f1801875bf6ea82036128082e4adb79691236faa
oplopa <plopaoctavian@yahoo.com>
2024-05-18 12:41:03 +0300

    adding first test

--- src/test/java/tests/TestReg.java
+++ src/test/java/tests/TestReg.java
@@ -2,11 +2,20 @@ package tests;
 
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class TestReg {
 
     @Test
-    public void TestNG(){
-        System.out.println("Hello world");
+    public void testListUsers(){
+        given()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .assertThat()
+                .statusCode(200);
+
+
 
     }
 }
